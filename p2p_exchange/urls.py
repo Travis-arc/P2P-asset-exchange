6de17b6c06e5c0f7c90566e4c6fd8d78b267f3c5
Cosmas David <cosmasdavid23@gmail.com>
2026-07-26 20:50:59 +0100

    Redirect homepage to listings

--- p2p_exchange/urls.py
+++ p2p_exchange/urls.py
@@ -2,8 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/listings/', permanent=False)),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('listings/', include('listings.urls')),
