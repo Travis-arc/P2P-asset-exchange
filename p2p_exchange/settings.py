4bb9064abf129a917daade8757e53eb4305840c4
Travis-arc <cosmasdavid23@gmail.com>
2026-09-01 12:35:01 +0100

    Disable persistent DB connections to avoid stale connections on serverless sleep/wake

--- p2p_exchange/settings.py
+++ p2p_exchange/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'p2p_exchange.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600,
+        conn_max_age=0,
     )
 }
 
