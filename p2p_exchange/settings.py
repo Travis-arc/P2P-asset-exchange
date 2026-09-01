64ba6eddda15848128f5b5376e3b67a6ef0dc375
Travis-arc <cosmasdavid23@gmail.com>
2026-09-01 11:59:21 +0100

    Add explicit logging so production errors appear in Railway logs

--- p2p_exchange/settings.py
+++ p2p_exchange/settings.py
@@ -164,3 +164,24 @@ else:
 LOGIN_URL = 'accounts:login'
 LOGIN_REDIRECT_URL = 'accounts:profile'
 LOGOUT_REDIRECT_URL = 'accounts:login'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
