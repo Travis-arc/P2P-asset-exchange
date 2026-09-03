5a189b2af8ff5f104c6759c1791c0f7893f347b4
Travis-arc <cosmasdavid23@gmail.com>
2026-09-03 12:30:35 +0100

    Show matric number in admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,7 +7,7 @@ from .models import CustomUser
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('email', 'username', 'department', 'is_email_verified',
+    list_display = ('email', 'username', 'matric_number', 'department', 'is_email_verified',
                      'reputation_score', 'is_staff')
     list_filter = ('is_email_verified', 'is_staff', 'department')
     search_fields = ('email', 'username', 'department')
@@ -15,7 +15,7 @@ class CustomUserAdmin(UserAdmin):
 
     fieldsets = (
         (None, {'fields': ('email', 'username', 'password')}),
-        ('Profile', {'fields': ('department',)}),
+        ('Profile', {'fields': ('department', 'matric_number')}),
         ('Verification & trust', {
             'fields': ('is_email_verified', 'reputation_score', 'total_ratings')
         }),
